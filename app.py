b2fe6e2a3d56544ddaa3ea9d0c391514333ad5ee
SayanjitDas-web <thewebman01@gmail.com>
2025-08-30 12:18:29 +0530

    app updated

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ prompt = st.chat_input("eneter query...")
 if prompt:
     user_message =st.chat_message("user")
     user_message.write(prompt)
+    user_message.write("ai is thinking..")
+    result = genrateWebsite(prompt)
+    if 'htmlvalue' not in st.session_state:
+            st.session_state['htmlvalue'] = result
+    st.page_link("pages/preview.py",label="preview")
